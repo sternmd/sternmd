ca59673c08dce92c7b5af473e26a9222357256a1
Max Stern <stern.maxwell@gmail.com>
2016-05-03 14:36:39 -0500

    desc.

--- portfolio/routes/index.js
+++ portfolio/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Maxwell Stern | Web Development & Design' });
+  res.render('index', { title: 'Maxwell Stern Portfolio | Web Development & Design' });
 });
 
 router.get('/contact', function(req, res, next) {
