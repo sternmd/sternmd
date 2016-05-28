2f4e982625ac2937d713a150cdd78c16d110f6a2
Max Stern <stern.maxwell@gmail.com>
2016-05-27 22:38:56 -0500

    core.hbs

--- portfolio/routes/index.js
+++ portfolio/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Maxwell Stern Portfolio | Web Development & Design' });
 });
 
+router.get('/core', function(req, res, next) {
+  res.render('core', { title: 'Maxwell Stern Portfolio | Favorite Graphic Novels' });
+});
+
+
 module.exports = router;
