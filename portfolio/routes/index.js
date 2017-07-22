0a281424ee01395a4f2eab3618f76d2be94ad747
Max Stern <stern.maxwell@gmail.com>
2017-07-22 13:01:23 -0500

    studiomosaic img

--- portfolio/routes/index.js
+++ portfolio/routes/index.js
@@ -3,12 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Maxwell Stern Portfolio | Web Development & Design' });
+  res.render('index', { title: 'Maxwell Stern | Web Development & Design' });
 });
-
-router.get('/core', function(req, res, next) {
-  res.render('core', { title: 'Maxwell Stern Portfolio | Favorite Graphic Novels' });
-});
-
 
 module.exports = router;
